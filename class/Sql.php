ee5cb0e86bc0b9e293e019b3dfd3d59baf2539f2
Camila Janssen <cajanssen@gmail.com>
2020-06-16 12:13:05 -0300

    Criamos a Classe Usuario

--- class/Sql.php
+++ class/Sql.php
@@ -12,14 +12,14 @@ class Sql extends PDO {
 		echo "Conectou ao BD...<br><br>";
 	}
 
-	private function setParams($statment, $parameters=array()){
+	private function setParams($statement, $parameters=array()){
 		foreach ($parameters as $key => $value) {
-			$statment->setParam($key, $value);	
+			$this->setParam($statement,$key, $value);	
 		}
 	}
 
-	private function setParam($statment, $key, $value){
-		$statment->bindParam($key,$value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key,$value);
 	}
 
 	public function query($rawQuery,$params=array()){
--- index.php
+++ index.php
@@ -4,8 +4,12 @@ echo "Estou em index.php"."<br>";
 
 require_once("config.php");
 
-$sql = new Sql();
+/*$sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
+
+$user = new Usuario();
+$user->loadById(1);
+echo $user;
 
 ?>
